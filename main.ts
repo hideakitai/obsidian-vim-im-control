667abdada41cfa86015fbf7375b95dae73886fd3
Hideaki Tai <hideaki.tai@gmail.com>
2026-04-05 14:27:43 +0900

    fix: trim stdout when caching IM state
    Why: exec/execSync stdout includes trailing newline, which can break
    commands that use the {{im}} placeholder in cmdOnInsertEnter
    What: Add .toString().trim() to imToRestore assignment in both async
    and sync InsertLeave handlers
    Confidence: high

--- main.ts
+++ main.ts
@@ -220,7 +220,7 @@ export default class VimImSwitcher extends Plugin {
 		if (this.isOnInsertEnterEnabled()) {
 			// if onInsertEnter is enabled, we need to get current IM first
 			this.runCommandAsync(this.setting.cmdGetCurrentIM).then((stdout: any) => {
-				this.imToRestore = stdout;
+				this.imToRestore = stdout.toString().trim();
 				console.debug(`im cached: ${this.imToRestore}`);
 
 				// then run onInsertLeave command
@@ -248,7 +248,7 @@ export default class VimImSwitcher extends Plugin {
 
 		if (this.isOnInsertEnterEnabled()) {
 			// if onInsertEnter is enabled, we need to get current IM first
-			this.imToRestore = this.runCommandSync(this.setting.cmdGetCurrentIM);
+			this.imToRestore = this.runCommandSync(this.setting.cmdGetCurrentIM).trim();
 			console.debug(`im cached: ${this.imToRestore}`);
 		}
 
